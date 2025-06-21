3d14c9909bfe18fa602a12c58ee0b8c4519bc057
GNi33 <theill182@gmail.com>
2025-06-21 18:16:53 +0200

    Move game of life logic to module

--- www/index.ts
+++ www/index.ts
@@ -1,88 +1,11 @@
-import init, { Cell, Universe } from "playground";
+import { initGameOfLife } from "./src/modules/game_of_life";
 
-const CELL_SIZE = 5;
-const GRID_COLOR = "#CCCCCC";
-const DEAD_COLOR = "#FFFFFF";
-const ALIVE_COLOR = "#000000";
-
-let wasm;
-wasm = await init();
-
-const memory: WebAssembly.Memory = wasm.memory;
-
-const canvas = document.getElementById("game-of-life-canvas") as HTMLCanvasElement | null;
-
-if (!canvas) {
-    throw new Error('Element with id "game-of-life-canvas" not found');
-}
-
-const universe: Universe = Universe.new("spaceship");
-const width: number = universe.width();
-const height: number = universe.height();
-
-canvas.height = (CELL_SIZE + 1) * height + 1;
-canvas.width = (CELL_SIZE + 1) * width + 1;
-
-const ctx: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
-
-const renderLoop = (): void => {
-    universe.tick();
-
-    drawCells();
-
-    requestAnimationFrame(renderLoop);
-};
-
-const drawGrid = (): void => {
-    ctx.beginPath();
-    ctx.strokeStyle = GRID_COLOR;
-
-    for (let i = 0; i <= width; i++) {
-        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
-        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
-    }
-
-    for (let j = 0; j <= height; j++) {
-        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
-        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
-    }
-
-    ctx.stroke();
-};
-
-const getIndex = (row: number, column: number): number => {
-    return row * width + column;
-}
-
-const drawCells = (): void => {
-    const cellsPtr: number = universe.cells();
-
-    // The number of bytes needed to store all cells as bits
-    const numBytes = Math.ceil((width * height) / 8);
-    const cells: Uint8Array = new Uint8Array(memory.buffer, cellsPtr, numBytes);
-
-    ctx.beginPath();
-
-    for (let row = 0; row < height; row++) {
-        for (let column = 0; column < width; column++) {
-            const idx: number = getIndex(row, column);
-
-            const byte = Math.floor(idx / 8);
-            const bit = idx % 8;
-            const isAlive = (cells[byte] & (1 << bit)) !== Cell.Dead;
-
-            ctx.fillStyle = isAlive ? ALIVE_COLOR : DEAD_COLOR;
-            ctx.fillRect(
-                column * (CELL_SIZE + 1) + 1,
-                row * (CELL_SIZE + 1) + 1,
-                CELL_SIZE,
-                CELL_SIZE
-            );
+initGameOfLife('game-of-life-canvas', 'random').then(
+    (gameOfLife) => {
+        if (gameOfLife) {
+            gameOfLife.start();
+        } else {
+            console.error("Failed to initialize Game of Life.");
         }
     }
-
-    ctx.stroke();
-};
-
-drawGrid();
-requestAnimationFrame(renderLoop);
+);
---
+++ www/src/modules/game_of_life.ts
@@ -0,0 +1,122 @@
+import init, { Cell, Universe } from "playground";
+
+const CELL_SIZE = 5;
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+class GameOfLife {
+    private memory: WebAssembly.Memory;
+    private ctx: CanvasRenderingContext2D;
+    private universe: Universe;
+    private width: number;
+    private height: number;
+
+    private anmitionFrameId: number | null = null;
+    private gridDrawn: boolean = false;
+
+    private constructor(
+        memory: WebAssembly.Memory,
+        ctx: CanvasRenderingContext2D,
+        universe: Universe,
+        width: number,
+        height: number
+    ) {
+        this.memory = memory;
+        this.ctx = ctx;
+        this.universe = universe;
+        this.width = width;
+        this.height = height;
+    }
+
+    static async create(canvasId: string, initialState: string | null): Promise<GameOfLife> {
+        const wasm = await init();
+        const memory: WebAssembly.Memory = wasm.memory;
+        const canvas = document.getElementById(canvasId) as HTMLCanvasElement | null;
+        if (!canvas) throw new Error("Element with id " + canvasId + " not found");
+
+        const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+        const universe = Universe.new(initialState || "default");
+        const width = universe.width();
+        const height = universe.height();
+        canvas.height = (CELL_SIZE + 1) * height + 1;
+        canvas.width = (CELL_SIZE + 1) * width + 1;
+
+        return new GameOfLife(memory, ctx, universe, width, height);
+    }
+
+    private getIndex(row: number, column: number): number {
+        return row * this.width + column;
+    }
+
+    private drawGrid(): void {
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = GRID_COLOR;
+        for (let i = 0; i <= this.width; i++) {
+            this.ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+            this.ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * this.height + 1);
+        }
+        for (let j = 0; j <= this.height; j++) {
+            this.ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+            this.ctx.lineTo((CELL_SIZE + 1) * this.width + 1, j * (CELL_SIZE + 1) + 1);
+        }
+        this.ctx.stroke();
+
+        this.gridDrawn = true;
+    }
+
+    private drawCells(): void {
+        const cellsPtr: number = this.universe.cells();
+        const numBytes = Math.ceil((this.width * this.height) / 8);
+        const cells: Uint8Array = new Uint8Array(this.memory.buffer, cellsPtr, numBytes);
+        this.ctx.beginPath();
+        for (let row = 0; row < this.height; row++) {
+            for (let column = 0; column < this.width; column++) {
+                const idx: number = this.getIndex(row, column);
+                const byte = Math.floor(idx / 8);
+                const bit = idx % 8;
+                const isAlive = (cells[byte] & (1 << bit)) !== Cell.Dead;
+                this.ctx.fillStyle = isAlive ? ALIVE_COLOR : DEAD_COLOR;
+                this.ctx.fillRect(
+                    column * (CELL_SIZE + 1) + 1,
+                    row * (CELL_SIZE + 1) + 1,
+                    CELL_SIZE,
+                    CELL_SIZE
+                );
+            }
+        }
+        this.ctx.stroke();
+    }
+
+    private renderLoop = (): void => {
+
+        this.anmitionFrameId = null;
+
+        this.universe.tick();
+        this.drawCells();
+
+        this.start();
+    };
+
+    public start(): void {
+
+        if(!this.gridDrawn) {
+            this.drawGrid();
+        }
+
+        if(this.anmitionFrameId == null) {
+            this.anmitionFrameId = requestAnimationFrame(this.renderLoop);
+        }
+    }
+
+    public stop(): void {
+        if (this.anmitionFrameId !== null) {
+            cancelAnimationFrame(this.anmitionFrameId);
+            this.anmitionFrameId = null;
+        }
+    }
+}
+
+export async function initGameOfLife(canvasId: string, initialState: string | null): Promise<GameOfLife> {
+    return await GameOfLife.create(canvasId, initialState);
+}
