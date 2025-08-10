1148837aa37c65c1c8758bf599ce6e6bdd3b7eee
GNi33 <theill182@gmail.com>
2025-08-10 04:16:47 +0200

    Refactor WebGL rendering to use external shaders and implement basic rendering

--- www/src/modules/render_context/render_webgl.ts
+++ www/src/modules/render_context/render_webgl.ts
@@ -1,5 +1,10 @@
 import type {RenderContextInterface} from "./render_context_interface.ts";
-import {mat4} from "gl-matrix";
+
+// @ts-ignore
+import shaderVert from "../webgl/shaders/vertex.glsl?raw";
+// @ts-ignore
+import shaderFrag from "../webgl/shaders/fragment.glsl?raw";
+import { initWebGLProgram } from "../webgl/shaders.ts";
 
 export default class RenderContextWebGL implements RenderContextInterface {
 
@@ -8,24 +13,8 @@ export default class RenderContextWebGL implements RenderContextInterface {
     private readonly width: number;
     private readonly height: number;
 
-    private buffers: { position: WebGLBuffer };
-    private programInfo: any;
-
-    private static readonly VERTEX_SHADER_SOURCE = `
-        attribute vec4 aVertexPosition;
-        uniform mat4 uModelViewMatrix;
-        uniform mat4 uProjectionMatrix;
-        void main() {
-          gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-        }
-      `;
-
-    private static readonly FRAGMENT_SHADER_SOURCE = `
-        precision mediump float;
-        void main() {
-          gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); // White color
-        }
-      `;
+    private readonly buffers: { position: WebGLBuffer, uv: WebGLBuffer };
+    private readonly programInfo: any;
 
     constructor(
         canvas: HTMLCanvasElement,
@@ -45,106 +34,102 @@ export default class RenderContextWebGL implements RenderContextInterface {
         this.width = width;
         this.height = height;
 
-        const shaderProgram = this.initShaderProgram(this.ctx);
+        const shaderProgram = initWebGLProgram(this.ctx, shaderVert, shaderFrag);
+
         this.programInfo = {
             program: shaderProgram,
             attribLocations: {
-                vertexPosition: this.ctx.getAttribLocation(shaderProgram, "aVertexPosition"),
+                positionLoc: this.ctx.getAttribLocation(shaderProgram, "a_position"),
+                textureCoords: this.ctx.getAttribLocation(shaderProgram, "a_texCoord")
             },
             uniformLocations: {
-                projectionMatrix: this.ctx.getUniformLocation(shaderProgram, "uProjectionMatrix"),
-                modelViewMatrix: this.ctx.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+                texture: this.ctx.getUniformLocation(shaderProgram, "u_texture")
             },
         };
 
         this.buffers = this.initBuffers(this.ctx);
 
+        // Tell WebGL how to pull out the positions from the position
+        // buffer into the vertexPosition attribute.
+        this.setPositionAttribute(this.ctx, this.buffers.position, this.programInfo);
+        this.setUVAttribute(this.ctx, this.buffers.uv, this.programInfo);
+
     }
 
     drawGrid(): void {
-
-        let gl = this.ctx;
-
-        this.drawScene(gl, this.programInfo, this.buffers);
-
+        // todo
     }
 
     drawCells(cellsPtr: number): void {
+
+        let gl = this.ctx;
+
         const numBytes = Math.ceil((this.width * this.height) / 8);
         const cells: Uint8Array = new Uint8Array(this.memory.buffer, cellsPtr, numBytes);
+
+        let gridWidth = this.width;
+        let gridHeight = this.height;
+
+        const input = new Uint8Array(gridWidth * gridHeight * 4);
+        for (let i = 0; i < gridWidth * gridHeight; i++) {
+            const alive = (cells[Math.floor(i / 8)] >> (i % 8)) & 1;
+            const v = alive ? 255 : 0; // Black for alive, white for dead
+            input[i * 4 + 0] = v;
+            input[i * 4 + 1] = v;
+            input[i * 4 + 2] = v;
+            input[i * 4 + 3] = 255;
+        }
+
+        const tex = this.createTextureWithGridData(gl, input, this.width, this.height);
+        const fb = this.createFramebuffer(gl);
+
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex, 0);
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, tex);
+
+        this.drawScene(gl, this.programInfo);
     }
 
-    private initShaderProgram(gl: WebGL2RenderingContext): WebGLProgram {
-        const vertexShader = this.loadShader(gl, gl.VERTEX_SHADER, RenderContextWebGL.VERTEX_SHADER_SOURCE);
-        if (!vertexShader) {
-            throw new Error("Failed to load vertex shader");
-        }
-
-        const fragmentShader = this.loadShader(gl, gl.FRAGMENT_SHADER, RenderContextWebGL.FRAGMENT_SHADER_SOURCE);
-
-        if (!fragmentShader) {
-            throw new Error("Failed to load fragment shader");
-        }
-
-        const shaderProgram = gl.createProgram();
-        gl.attachShader(shaderProgram, vertexShader);
-        gl.attachShader(shaderProgram, fragmentShader);
-        gl.linkProgram(shaderProgram);
-
-        if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-            throw new Error("Unable to initialize the shader program: " + gl.getProgramInfoLog(shaderProgram));
-        }
-
-        return shaderProgram;
-
-    }
-
-    private loadShader(gl: WebGL2RenderingContext, type: any, source: any) {
-        const shader = gl.createShader(type);
-
-        if(!shader) {
-            throw new Error("Unable to create shader");
-        }
-
-        // Send the source to the shader object
-
-        gl.shaderSource(shader, source);
-
-        // Compile the shader program
-
-        gl.compileShader(shader);
-
-        // See if it compiled successfully
-
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            alert(
-                `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`,
-            );
-            gl.deleteShader(shader);
-            return null;
-        }
-
-        return shader;
-    }
-
-    private initBuffers(gl: WebGL2RenderingContext): { position: WebGLBuffer } {
+    private initBuffers(gl: WebGL2RenderingContext): { position: WebGLBuffer; uv: WebGLBuffer } {
         const positionBuffer = this.initPositionBuffer(gl);
+        const uvBuffer = this.initUVBuffer(gl);
 
         return {
             position: positionBuffer,
+            uv: uvBuffer,
         };
     }
 
+    private drawScene(gl: WebGL2RenderingContext, programInfo: any) {
+        // Tell WebGL to use our program when drawing
+        gl.useProgram(programInfo.program);
+
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    }
+
     private initPositionBuffer(gl: WebGL2RenderingContext): WebGLBuffer {
+
+        // create an array of positions for the square.
+        const positions = [
+            -1.0, -1.0,
+            -1.0,  1.0,
+             1.0, -1.0,
+             1.0,  1.0,
+            -1.0,  1.0,
+             1.0, -1.0,
+        ];
+
         // Create a buffer for the square's positions.
         const positionBuffer = gl.createBuffer();
 
         // Select the positionBuffer as the one to apply buffer
         // operations to from here out.
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-        // Now create an array of positions for the square.
-        const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
 
         // Now pass the list of positions into WebGL to build the
         // shape. We do this by creating a Float32Array from the
@@ -154,90 +139,114 @@ export default class RenderContextWebGL implements RenderContextInterface {
         return positionBuffer;
     }
 
-    private drawScene(gl: WebGL2RenderingContext, programInfo: any, buffers: any) {
-        gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
-        gl.clearDepth(1.0); // Clear everything
-        gl.enable(gl.DEPTH_TEST); // Enable depth testing
-        gl.depthFunc(gl.LEQUAL); // Near things obscure far things
+    private initUVBuffer(gl: WebGL2RenderingContext): WebGLBuffer {
 
-        // Clear the canvas before we start drawing on it.
+        // Define UV coordinates for a fullscreen quad
+        const uvCoordinates = [
+            0.0, 0.0, // Bottom-left
+            0.0, 1.0, // Top-left
+            1.0, 0.0, // Bottom-right
+            1.0, 1.0, // Top-right
+            0.0, 1.0,
+            1.0, 0.0, // Repeat the last two for the triangle strip
+        ];
 
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        // Create a buffer for the UV coordinates
+        const uvBuffer = gl.createBuffer();
 
-        // Create a perspective matrix, a special matrix that is
-        // used to simulate the distortion of perspective in a camera.
-        // Our field of view is 45 degrees, with a width/height
-        // ratio that matches the display size of the canvas
-        // and we only want to see objects between 0.1 units
-        // and 100 units away from the camera.
+        // Bind the buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
 
-        const fieldOfView = (45 * Math.PI) / 180; // in radians
-        const aspect = this.width / this.height;
-        const zNear = 0.1;
-        const zFar = 100.0;
-        const projectionMatrix = mat4.create();
+        // Pass the UV data to the buffer
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvCoordinates), gl.STATIC_DRAW);
 
-        // note: glMatrix always has the first argument
-        // as the destination to receive the result.
-        mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+        return uvBuffer;
+    }
 
-        // Set the drawing position to the "identity" point, which is
-        // the center of the scene.
-        const modelViewMatrix = mat4.create();
+    private setUVAttribute(gl: WebGL2RenderingContext, uvBuffer: WebGLBuffer, programInfo: any) {
+        const numComponents = 2; // 2 values per UV coordinate
+        const type = gl.FLOAT; // 32-bit float
+        const normalize = false; // Don't normalize
+        const stride = 0; // No stride
+        const offset = 0; // Start at the beginning of the buffer
 
-        // Now move the drawing position a bit to where we want to
-        // start drawing the square.
-        mat4.translate(
-            modelViewMatrix, // destination matrix
-            modelViewMatrix, // matrix to translate
-            [-0.0, 0.0, -6.0],
-        ); // amount to translate
+        // Bind the UV buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
 
-        // Tell WebGL how to pull out the positions from the position
-        // buffer into the vertexPosition attribute.
-        this.setPositionAttribute(gl, buffers, programInfo);
+        // Set up the attribute pointer
+        gl.vertexAttribPointer(programInfo.attribLocations.textureCoords, numComponents, type, normalize, stride, offset);
 
-        // Tell WebGL to use our program when drawing
-        gl.useProgram(programInfo.program);
-
-        // Set the shader uniforms
-        gl.uniformMatrix4fv(
-            programInfo.uniformLocations.projectionMatrix,
-            false,
-            projectionMatrix,
-        );
-        gl.uniformMatrix4fv(
-            programInfo.uniformLocations.modelViewMatrix,
-            false,
-            modelViewMatrix,
-        );
-
-        {
-            const offset = 0;
-            const vertexCount = 4;
-            gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
-        }
+        // Enable the attribute
+        gl.enableVertexAttribArray(programInfo.attribLocations.textureCoords);
     }
 
     // Tell WebGL how to pull out the positions from the position
-// buffer into the vertexPosition attribute.
-    private setPositionAttribute(gl: WebGL2RenderingContext, buffers: any, programInfo: any) {
+    // buffer into the vertexPosition attribute.
+    private setPositionAttribute(gl: WebGL2RenderingContext, buffer: WebGLBuffer, programInfo: any) {
         const numComponents = 2; // pull out 2 values per iteration
         const type = gl.FLOAT; // the data in the buffer is 32bit floats
         const normalize = false; // don't normalize
         const stride = 0; // how many bytes to get from one set of values to the next
         // 0 = use type and numComponents above
         const offset = 0; // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
         gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexPosition,
+            programInfo.attribLocations.positionLoc,
             numComponents,
             type,
             normalize,
             stride,
             offset,
         );
-        gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+        gl.enableVertexAttribArray(programInfo.attribLocations.positionLoc);
+    }
+
+    private createTexture(gl: WebGLRenderingContext) {
+        const tex = gl.createTexture();
+        if (tex === null) {
+            throw Error("failed to create texture");
+        }
+
+        gl.bindTexture(gl.TEXTURE_2D, tex);
+
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        return tex;
+    }
+
+    private createTextureWithGridData(
+        gl: WebGLRenderingContext,
+        arr: ArrayBufferView | null,
+        width: number,
+        height: number
+    ) {
+        const srcWidth = width;
+        const srcHeight = height;
+
+        const tex = this.createTexture(gl);
+
+        gl.texImage2D(
+            gl.TEXTURE_2D,
+            0,                // mip level
+            gl.RGBA,          // internal format
+            srcWidth,
+            srcHeight,
+            0,                // border
+            gl.RGBA,          // format
+            gl.UNSIGNED_BYTE, // type
+            arr);
+        return tex;
+    }
+
+    private createFramebuffer(gl: WebGLRenderingContext): WebGLFramebuffer {
+        const fb = gl.createFramebuffer();
+        if (fb === null) {
+            throw Error("failed to create framebuffer");
+        }
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+        return fb;
     }
 
 }
